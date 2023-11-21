4fd5b53992dd45768e59ade0dfc14ab2ca0747b7
Роман Сергунцов <serguntsov@MacBook-Pro-Roman.local>
2023-11-21 17:40:00 +0300

    lab4 processed 70

--- lab4/CopyPaste.java
+++ lab4/CopyPaste.java
@@ -1,21 +1,31 @@
 package lab4;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class CopyPaste {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File file = new File("/Users/serguntsov/desktop/java/lab/src/lab4/copy.txt");
+        File filenew = new File("/Users/serguntsov/desktop/java/lab/src/lab4/paste.txt");
         try{
+            filenew.createNewFile();
             Scanner scan = new Scanner(file);
+            FileWriter writer = new FileWriter("/Users/serguntsov/desktop/java/lab/src/lab4/paste.txt");
             while (scan.hasNextLine()){
-                System.out.println(scan.nextLine());
+                String line = scan.nextLine();
+                writer.write(line);
+                System.out.println(line);
             }
             scan.close();
+            writer.close();
         } catch (FileNotFoundException e){
             System.out.println("File Not Founded");
         } catch (java.lang.IllegalStateException e){
             System.out.println("Reading error");
+        } catch (IOException e){
+            System.out.println("Write error");
         }
     }
 }
--- lab4/CustomFileNotFoundException.java
+++ lab4/CustomFileNotFoundException.java
@@ -2,6 +2,8 @@ package lab4;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -9,7 +11,10 @@ public class CustomFileNotFoundException {
     public static void main(String[] args) {
         String line = "";
         File file = new File("/Users/serguntsov/desktop/java/lab/src/lab4/parseint.txt");
+        File filenew = new File("/Users/serguntsov/desktop/java/lab/src/lab4/log.txt");
         try{
+            filenew.createNewFile();
+            FileWriter writer = new FileWriter("/Users/serguntsov/desktop/java/lab/src/lab4/paste.txt");
             Scanner scan = new Scanner(file);
             line = scan.nextLine().replaceAll("\\pP", "");
             scan.close();
@@ -21,9 +26,12 @@ public class CustomFileNotFoundException {
             }
             System.out.println(Arrays.toString(num));
         } catch (FileNotFoundException e){
+            writer.write(line);
             System.out.println("File Not Founded");
         } catch (java.lang.NumberFormatException e){
             System.out.println("Number Format Error: " + Arrays.toString(line.split(" ")));
+        } catch (IOException e) {
+            System.out.println("Write error");
         }
     }
 }
